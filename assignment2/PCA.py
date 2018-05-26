abb78343963c1a16488bdd2b94937da86e116fa2
Tim van Rossum <trvanrossum@gmail.com>
2018-05-26 15:36:17 +0200

    Started working on PCA, first setting data to zero mean.

--- assignment2/PCA.py
+++ assignment2/PCA.py
@@ -1,6 +1,17 @@
 from assignment2.io import read_csv_adapted
+import pandas as pd
 
 key = 'F_PU2'
+
+def set_to_zero_mean(df):
+    data = []
+    index = df.index
+    mean = df.mean(axis=0).values
+    columns = df.columns
+    for row in df.iterrows():
+        r = row[1].values - mean
+        data.append(r)
+    res = pd.DataFrame(index=index, data=data, columns=columns)
 
 # Read data.
 series = read_csv_adapted('data/BATADAL_train_dataset_1.csv')
@@ -9,3 +20,5 @@ series = read_csv_adapted('data/BATADAL_train_dataset_1.csv')
 # (Set end to 8762 and start to 0 for all).
 start = 0
 end = 8700
+
+set_to_zero_mean(series)
