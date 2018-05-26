bd2c05c2ced537b5b71924d58c975f38dcfc4eb3
Tim van Rossum <trvanrossum@gmail.com>
2018-05-26 15:56:41 +0200

    PCA works now. I think we can use this for point anomaly detection.

--- assignment2/PCA.py
+++ assignment2/PCA.py
@@ -1,5 +1,6 @@
 from assignment2.io import read_csv_adapted
 import pandas as pd
+from sklearn.decomposition import PCA
 
 key = 'F_PU2'
 
@@ -11,7 +12,15 @@ def set_to_zero_mean(df):
     for row in df.iterrows():
         r = row[1].values - mean
         data.append(r)
-    res = pd.DataFrame(index=index, data=data, columns=columns)
+    return pd.DataFrame(index=index, data=data, columns=columns)
+
+def PCA_decompose(df, comps, solver='auto'):
+    data = df.values
+    index = df.index
+    columns = ['Component %i' % x for x in range(1, comps + 1)]
+    pca = PCA(n_components=comps, svd_solver=solver)
+    new_data = pca.fit_transform(data)
+    return pd.DataFrame(index=index, data=new_data, columns=columns)
 
 # Read data.
 series = read_csv_adapted('data/BATADAL_train_dataset_1.csv')
@@ -21,4 +30,7 @@ series = read_csv_adapted('data/BATADAL_train_dataset_1.csv')
 start = 0
 end = 8700
 
-set_to_zero_mean(series)
+new_series = set_to_zero_mean(series)
+new_series.drop('ATT_FLAG', axis=1, inplace=True)
+
+decomposed_data = PCA_decompose(new_series, 2)
