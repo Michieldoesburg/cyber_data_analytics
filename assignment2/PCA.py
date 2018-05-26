3305eebf87526c52cb4b984c83fff95b6bffcb8f
Tim van Rossum <trvanrossum@gmail.com>
2018-05-26 17:43:38 +0200

    Point anomaly detection now somewhat correctly implemented I guess?

--- assignment2/PCA.py
+++ assignment2/PCA.py
@@ -1,6 +1,8 @@
 from assignment2.io import read_csv_adapted
 import pandas as pd
 from sklearn.decomposition import PCA
+from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM
 from matplotlib import pyplot
 
 key = 'F_PU2'
@@ -23,18 +25,44 @@ def PCA_decompose(df, comps, solver='auto'):
     new_data = pca.fit_transform(data)
     return pd.DataFrame(index=index, data=new_data, columns=columns)
 
+def set_zero_mean_decompose(df, comps, keys_to_drop, solver='auto'):
+    new_df = df
+    new_df.drop(keys_to_drop, axis=1)
+    new_df = set_to_zero_mean(new_df)
+    return PCA_decompose(new_df, comps, solver)
+
+def generate_list_outliers(train_data, test_data, test_data_indices, clf):
+    predictions = clf.fit(train_data).predict(test_data)
+    outlier_indices = list()
+    for i in range(len(predictions)):
+        if predictions[i] == -1:
+            outlier_indices.append(test_data_indices[i])
+
+    return outlier_indices
+
+
 # Read data.
-series = read_csv_adapted('data/BATADAL_train_dataset_1.csv')
+train_data = read_csv_adapted('data/BATADAL_train_dataset_1.csv')
+test_data = read_csv_adapted('data/BATADAL_test_dataset.csv')
 
-# Select start and stop indices for a given data range.
-# (Set end to 8762 and start to 0 for all).
-start = 0
-end = 8700
+decomposed_train_data = set_zero_mean_decompose(train_data, 7, 'ATT_FLAG')
+decomposed_test_data = set_zero_mean_decompose(test_data, 7, [])
 
-new_series = set_to_zero_mean(series)
-new_series.drop('ATT_FLAG', axis=1, inplace=True)
+print('Generating outliers with the isolation forest model.')
+# Notes regarding parameters:
+#   - Contamination set to 0, as training data does not contain any attacks.
+#   - max_samples set to all samples, as that provided more accurate results.
+outlier_indices = generate_list_outliers(decomposed_train_data.values, decomposed_test_data.values, decomposed_test_data.index, IsolationForest(contamination=0, max_samples=8761))
+print('Amount of outliers found: %i' % len(outlier_indices))
+print('List of outliers:')
+print(outlier_indices)
+print('Generating outliers with the one-class SVM model.')
+# Notes regarding parameters:
+#   - Kernel set to sigmoid, seems to be the best option from several that we tried.
+outlier_indices = generate_list_outliers(decomposed_train_data.values, decomposed_test_data.values, decomposed_test_data.index, OneClassSVM(kernel='sigmoid'))
+print('Amount of outliers found: %i' % len(outlier_indices))
+print('List of outliers:')
+print(outlier_indices)
 
-decomposed_data = PCA_decompose(new_series, 2)
-
-pyplot.plot(decomposed_data[decomposed_data.columns][0:1000])
-pyplot.show()
+# pyplot.plot(decomposed_train_data[decomposed_train_data.columns][0:1000])
+# pyplot.show()
