cfc7d6bddbde325aab268aaf6bebacd48a9e27ed
Tim van Rossum <trvanrossum@gmail.com>
2018-06-11 16:29:07 +0200

    Made some diagnostics functions.

--- assignment3/sampling.py
+++ assignment3/sampling.py
@@ -9,7 +9,8 @@ addresses = []
 id_to_ip_map = dict()
 ip_freq = dict()
 ip_amt = 0
-reservoir_size = 1000
+reservoir_size = 10000
+k = 10
 sample = MinWiseSample(reservoir_size)
 
 def sort_dict_by_value(freq, max_val):
@@ -24,6 +25,29 @@ def sort_dict_by_value(freq, max_val):
         # Apply correction to get the actual frequencies back.
         res[x[1]] = max_val - x[0]
     return res
+
+def select_first_k(dictionary, k):
+    keyset = list(dictionary.keys())
+    res = dict()
+    for i in range(k):
+        key = keyset[i]
+        res[key] = dictionary[key]
+    return res
+
+def keys_in_common(dict1, dict2):
+    count = 0
+    for k in dict1:
+        if k in dict2:
+            count += 1
+    return count
+
+def keys_in_same_position(dict1, dict2):
+    count = 0
+    keys1, keys2 = list(dict1.keys()), list(dict2.keys())
+    for i in range(len(keys1)):
+        if keys1[i] == keys2[i]:
+            count += 1
+    return count
 
 
 # Treat data as a stream.
@@ -58,10 +82,27 @@ with open("data\capture20110816-2.pcap.netflow.labeled", "r") as f:
                 print(ip)
 
 
+total_freq_sorted = sort_dict_by_value(ip_freq, ip_amt)
+sampled_freq_sorted = sample.count_and_sort()
+
+print('Stream reading done.')
+print('There are %i destination IP addresses' % ip_amt)
+
+print('Printing all frequencies:')
 print('Total frequencies, sorted by value in descending order:')
-print(sort_dict_by_value(ip_freq, ip_amt))
+print(total_freq_sorted)
 print('Frequencies of samples sampled by using min-wise sampling with a reservoir size of %i, sorted by value in descending order:' % reservoir_size)
-print(sample.count_and_sort())
+print(sampled_freq_sorted)
+print('')
+first_k_all = select_first_k(total_freq_sorted, k)
+first_k_sampled = select_first_k(sampled_freq_sorted, k)
+print('Printing only the top %i frequencies' % k)
+print('Top %i highest frequencies of the total frequencies:' % k)
+print(first_k_all)
+print('Top %i highest frequencies of the sampled frequencies:' % k)
+print(first_k_sampled)
+print('These last two sets have %i IP addresses in common, %i of which are in the same position' % (keys_in_common(first_k_all, first_k_sampled), keys_in_same_position(first_k_all, first_k_sampled)))
+
 
 max_id = 0
 
