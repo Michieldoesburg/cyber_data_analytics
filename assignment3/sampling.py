7f70347b9ee454986ba97d729f2537894fa0723a
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2018-06-11 11:07:57 +0200

    read the ip addresses.

--- assignment3/sampling.py
+++ assignment3/sampling.py
@@ -1,8 +1,20 @@
 import csv
 
+addresses = []
+
 with open("data\capture20110816-2.pcap.netflow.labeled", "r") as f:
     reader = csv.reader(f, delimiter=" ")
-    for i, line in enumerate(reader):
-        strings = line[1].split(" ")
-        strings = strings[0].split("\t")
-        print('line[{}] = {}'.format(i, strings[0]))
+    for z, line in enumerate(reader):
+        if z < 1:
+            continue
+
+        # Split the arguments in the line
+        args = line[1].split("\t")
+
+        for i, x in enumerate(args):
+            # We are interested in the ip address after the -> arrow.
+            if x == "->":
+                # Split the IP address from the port number
+                ip = args[i+1].split(":")[0]
+                addresses.append(ip)
+                print(ip)
