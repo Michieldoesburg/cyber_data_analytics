f55da4cb6c6d8002c701f16d8c6069d4a01e28ce
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2018-06-11 10:56:54 +0200

    reading the file.

--- assignment3/sampling.py
+++ assignment3/sampling.py
@@ -1,5 +1,8 @@
-from scapy.all import *
+import csv
 
-packets = rdpcap('data/capture20110818-2.pcap.netflow.labeled')
-
-print(packets)
+with open("data\capture20110816-2.pcap.netflow.labeled", "r") as f:
+    reader = csv.reader(f, delimiter=" ")
+    for i, line in enumerate(reader):
+        strings = line[1].split(" ")
+        strings = strings[0].split("\t")
+        print('line[{}] = {}'.format(i, strings[0]))
