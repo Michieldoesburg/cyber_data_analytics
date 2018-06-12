413a005f184209fedc3ffc289520efb3bbbb5e2b
Tim van Rossum <trvanrossum@gmail.com>
2018-06-13 00:23:45 +0200

    The third question is going to be stupid easy tbh.

---
+++ assignment3/discretization.py
@@ -0,0 +1,36 @@
+import csv
+from assignment3.utils import *
+from assignment3.packet import *
+
+file = "data\CTU13-scenario10.pcap.netflow.labeled"
+
+filtered_packets = []
+count_skipped = 0
+
+# Treat data as a stream.
+with open(file, "r") as f:
+    reader = csv.reader(f, delimiter=" ")
+    for z, line in enumerate(reader):
+        if z < 1:
+            continue
+
+        # Split the arguments in the line
+        args = line[1].split("\t")
+        new_args = remove_empty_strings(args)
+        if len(new_args) < 12:
+            count_skipped += 1
+            continue
+        # print(new_args)
+        date = line[0] + ' ' + new_args[0]
+        p = packet(date, new_args[1], new_args[2], new_args[3], new_args[5], new_args[6], new_args[7], new_args[8], new_args[9], new_args[10], new_args[11])
+
+        ip = p.dst.split(":")[0]
+        label = p.label
+
+        # Filter the broadcasts and non-ip adresses
+        if (ip != "Broadcast") and (ip != "ff02") and (label != 'Background'):
+            filtered_packets.append(p)
+
+print(count_skipped)
+for x in filtered_packets:
+    print(x)
