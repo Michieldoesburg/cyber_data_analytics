5a402f6a65bc933402e333cbff85722803596d35
Michiel Doesburg <M.S.Doesburg@student.tudelft.nl>
2018-06-14 11:39:20 +0200

    sampling for different reservoir sizes.

--- assignment3/sampling.py
+++ assignment3/sampling.py
@@ -3,12 +3,17 @@ from assignment3.min_wise_sample import MinWiseSample
 from assignment3.utils import *
 from assignment3.packet import *
 
+reservoir_size_range = [100, 1000, 10000, 100000]
+
+samples = []
 addresses = []
 ip_freq = dict()
 ip_amt = 0
-reservoir_size = 10000
 k = 10
-sample = MinWiseSample(reservoir_size)
+
+for x in reservoir_size_range:
+    samples.append(MinWiseSample(x))
+
 file = "data\capture20110816-2.pcap.netflow.labeled"
 
 # Treat data as a stream.
@@ -29,7 +34,8 @@ with open(file, "r") as f:
         # Filter the broadcasts and non-ip adresses
         if (ip != "Broadcast") and (ip != "ff02"):
             # Add to the min-wise sampling pool.
-            sample.add(ip)
+            for sample in samples:
+                sample.add(ip)
             # Add to the frequency counter.
             if ip in ip_freq:
                 ip_freq[ip] += 1
@@ -41,23 +47,30 @@ with open(file, "r") as f:
 
 
 total_freq_sorted = sort_dict_by_value(ip_freq, ip_amt)
-sampled_freq_sorted = sample.count_and_sort()
+for i, sample in enumerate(samples):
+    samples[i] = sample.count_and_sort()
 
 print('Stream reading done.')
 print('The file that was read can be found in %s' % file)
 print('There are %i destination IP addresses' % ip_amt)
 
-print('Printing all frequencies:')
-print('Total frequencies, sorted by value in descending order:')
-print(total_freq_sorted)
-print('Frequencies of samples sampled by using min-wise sampling with a reservoir size of %i, sorted by value in descending order:' % reservoir_size)
-print(sampled_freq_sorted)
+# print('Printing all frequencies:')
+# print('Total frequencies, sorted by value in descending order:')
+# print(total_freq_sorted)
+#
+# for i, sample in enumerate(samples):
+#     print('Frequencies of samples sampled by using min-wise sampling with a reservoir size of %i, sorted by value in descending order:' % 10**(i+2))
+#     print(sample)
+
 print('')
 first_k_all = select_first_k(total_freq_sorted, k)
-first_k_sampled = select_first_k(sampled_freq_sorted, k)
 print('Printing only the top %i frequencies' % k)
 print('Top %i highest frequencies of the total frequencies:' % k)
 print(first_k_all)
-print('Top %i highest frequencies of the sampled frequencies:' % k)
-print(first_k_sampled)
-print('These last two sets have %i IP addresses in common, %i of which are in the same position' % (keys_in_common(first_k_all, first_k_sampled), keys_in_same_position(first_k_all, first_k_sampled)))
+
+for i, sample in enumerate(samples):
+    first_k_sampled = select_first_k(sample, k)
+    print('Top {} highest frequencies of the sampled frequencies with reservoir size {}:'.format(k,  10**(i+2)))
+    print(first_k_sampled)
+    print('\n')
+    print('These last two sets have %i IP addresses in common, %i of which are in the same position' % (keys_in_common(first_k_all, first_k_sampled), keys_in_same_position(first_k_all, first_k_sampled)))
